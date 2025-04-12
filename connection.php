8bed60fc6f99203f0f2acf9a00002da78cdeb7a4
Ibogr <107372331+Ibogr@users.noreply.github.com>
2025-04-12 15:35:59 +0100

    update php

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+echo"php is working...";
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$conn=mysqli_connect("localhost", "root", "", "photoBlog");
+mysqli_set_charset($conn,"UTF8");
+
+if (mysqli_connect_errno() > 0) {
+    die("No Connection " . mysqli_connect_errno());
+  }
+
+?>
