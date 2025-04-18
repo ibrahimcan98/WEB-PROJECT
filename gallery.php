d776128f580ae97db13812b01274bc4625a67419
ne-el-al <e.a.negreeva@gmail.com>
2025-04-18 18:33:11 +0100

    comments to files

--- gallery.php
+++ gallery.php
@@ -1,33 +1,35 @@
 <?php
-$page_title = "Gallery";
-include("./nav.php");
+$page_title = "Gallery"; // page title
+include("./nav.php"); //include header of the page
 include("./connection.php");
 
-$query = $conn->prepare("SELECT filePath FROM Photos;");
-$query->execute();
-$result = $query->get_result();
-//var_dump($result);
-$user = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$query = $conn->prepare("SELECT filePath FROM Photos;"); // SQL to get all files from photos database
+$query->execute(); // executes query
+$result = $query->get_result(); // creates an object of results
+//var_dump($result); 
+$user = mysqli_fetch_all($result, MYSQLI_ASSOC); // makes results appear as array
 //var_dump($user);
 ?>
-<section class="gallery_page">
+<section class="gallery_page"> 
 <h1>Gallery</h1>
 <figure class="gallery">
 <?php
-// trouble with horizontal images
+// takes each element of the user array
 foreach ($user as $img) {
     //var_dump($img);
-    $size = getimagesize($img["filePath"]);
+    $size = getimagesize($img["filePath"]); // gets width and height of the photo
+    // if width is more than height, it assigns horizontal class
+    // otherwise image has vertical class
     if ($size[0] > $size[1]) {
-        echo '<img class="horizontal" src="'. $img["filePath"] . '" alt="" />';
+        echo '<img class="horizontal" src="'. $img["filePath"] . '" alt="" />'; // show image from found filepath
     }
     else
-        echo '<img class="vertical" src="'. $img["filePath"] . '" alt="" />';
+        echo '<img class="vertical" src="'. $img["filePath"] . '" alt="" />'; // show image from found filepath
    
 }
 ?>
 </figure>
 </section>
 <?php
-include("./footer.php");
+include("./footer.php"); //include footer of the page
 ?>
