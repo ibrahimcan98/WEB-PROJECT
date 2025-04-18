4b25fc13ec4bb2844ac70416e44cff57736dcaa1
ne-el-al <e.a.negreeva@gmail.com>
2025-04-18 05:04:34 +0100

    gallery page created and modified

--- gallery.php
+++ gallery.php
@@ -1,10 +1,27 @@
 <?php
 $page_title = "Gallery";
 include("./nav.php");
+include("./connection.php");
+
+$query = $conn->prepare("SELECT filePath FROM Photos;");
+$query->execute();
+$result = $query->get_result();
+$user = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
-
-
-
+<figure class="gallery">
+<?php
+// trouble with horizontal images
+foreach ($user as $img) {
+    $size = getimagesize($img["filePath"]);
+    if ($size[0] > $size[1]) {
+        //echo '<img class="horizontal" src="'. $img["filePath"] . '" alt="" />';
+    }
+    else
+        echo '<img class="vertical" src="'. $img["filePath"] . '" alt="" />';
+   
+}
+?>
+</figure>
 <?php
 include("./footer.php");
 ?>
